4a6620108fde557cc2bc50094ddb6bb2ff47ea23
gifary10 <57998309+gifary10@users.noreply.github.com>
2025-11-14 08:05:45 +0700

    Modify CONFIG URLs in config.js
    Updated URLs for data master APAR and inspection storage.

--- js/config.js
+++ js/config.js
@@ -1,17 +1,9 @@
 export const CONFIG = {
   // URL untuk data master APAR dan riwayat pemeriksaan (code1.gs)
-  scriptURL: 'https://script.google.com/macros/s/AKfycbwnuPxLD0yw9Y7u8phaDSBnlSmH_KCbrsBLHUMPSYrk9vOuHBBuQwWVbHTBkFDhMn6F/exec',
+  scriptURL: 'https://script.google.com/macros/s/AKfycbzPlt2aiGy6xi1L7vZ6_P7X8wO69Nmp_h8xhw9vT_PmGAl133Ht-rHqfNBz38LxWK34/exec',
   
   // URL untuk menyimpan data inspeksi (code2.gs)
-  inspectionSheetURL: 'https://script.google.com/macros/s/AKfycbybZYBwE9r6BJbscqP-d3XZ2baRbhOT_JfHYmoFgayqV9Zsc-_josMNnguY4LZ2bPZV/exec',
+  inspectionSheetURL: 'https://script.google.com/macros/s/AKfycbwx26SEsNP_dxbt0949Sv5WCdotrGtuFW2FIXTZaXA8lSHOuKUSGTurp7avuglZQSQi/exec',
   
-  defaultDate: new Date().toISOString().split('T')[0],
-  
-  // Konfigurasi untuk data master APAR
-  aparSheetId: '18usF1zfMJZh0ZBsWribR4uvFYGjQAbM_3Y4up6xM7us',
-  aparSheetName: 'Data',
-  
-  // Konfigurasi untuk data riwayat pemeriksaan
-  inspectionSheetId: '18usF1zfMJZh0ZBsWribR4uvFYGjQAbM_3Y4up6xM7us',
-  inspectionHistorySheetName: 'Rekap'
+  defaultDate: new Date().toISOString().split('T')[0]
 };
